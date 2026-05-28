15eedf1896eb196dc1e19dda4d61a4b00e55e2e1
Tấn Đạt <1thangnoob@gmail.com>
2026-05-28 14:06:05 +0700

    Update ngày 28/05/2026: - Vừa sửa lỗi project bị xung đột

---
+++ Services/LoginService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transaction_Management.Models;
+
+namespace Transaction_Management.Services
+{
+    internal class LoginService
+    {
+        public bool Authenticate(string username, string password, out int roleID)
+        {
+            roleID = 0;
+            using (var db = new TM_Database())
+            {
+                var user = db.Users
+                             .FirstOrDefault(u => u.Username == username
+                                               && u.PasswordHash == password);
+                if (user == null) return false;
+
+                roleID = (int)user.RoleID;
+                UserSessionService.CurrentUser = user;
+                return true;
+            }
+        }
+    }
+}
